88a5a1f464ced3259155a0b8ff54cf7509aa602c
LCostaCoutinho <lc.coutinho@outlook.com>
2017-04-08 13:05:00 -0300

    building arrearages object

--- app/controllers/api/v1/arrearages_controller.rb
+++ app/controllers/api/v1/arrearages_controller.rb
@@ -1,2 +1,28 @@
-class Api::V1::ArrearagesController < ApplicationController
+class Api::V1::ArrearagesController < Api::V1::BaseController
+
+  def index
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_arrearage
+    @current_resource.find(params[:id])
+  end
+
+  def arrearage_params
+    params.permit(:receiver_id)
+  end
+
 end
